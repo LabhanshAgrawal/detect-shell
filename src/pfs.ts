c05c7e65a01f2dcaf122afb575f584cab678aa1d
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2021-01-28 12:48:16 +0530

    Incorporate changes from vscode

--- src/pfs.ts
+++ src/pfs.ts
@@ -25,36 +25,70 @@ export async function readdir(path: string): Promise<string[]> {
 
 export async function dirExists(path: string): Promise<boolean> {
   try {
-    const fileStat = await stat(path);
+    const {stat, symbolicLink} = await statLink(path);
 
-    return fileStat.isDirectory();
+    return stat.isDirectory() && symbolicLink?.dangling !== true;
   } catch (error) {
-    // This catch will be called on some symbolic links on Windows (AppExecLink for example).
-    // So we try our best to see if it's a Directory.
-    try {
-      const fileStat = await stat(await readlink(path));
-
-      return fileStat.isDirectory();
-    } catch {
-      return false;
-    }
+    // Ignore, path might not exist
   }
+
+  return false;
 }
 
 export async function fileExists(path: string): Promise<boolean> {
   try {
-    const fileStat = await stat(path);
+    const {stat, symbolicLink} = await statLink(path);
 
-    return fileStat.isFile();
+    return stat.isFile() && symbolicLink?.dangling !== true;
   } catch (error) {
-    // This catch will be called on some symbolic links on Windows (AppExecLink for example).
-    // So we try our best to see if it's a File.
-    try {
-      const fileStat = await stat(await readlink(path));
-
-      return fileStat.isFile();
-    } catch {
-      return false;
-    }
+    // Ignore, path might not exist
   }
+
+  return false;
+}
+
+export async function statLink(path: string): Promise<IStatAndLink> {
+  // First stat the link
+  let lstats: fs.Stats | undefined;
+  try {
+    lstats = await lstat(path);
+
+    // Return early if the stat is not a symbolic link at all
+    if (!lstats.isSymbolicLink()) {
+      return {stat: lstats};
+    }
+  } catch (error) {
+    /* ignore - use stat() instead */
+  }
+
+  // If the stat is a symbolic link or failed to stat, use fs.stat()
+  // which for symbolic links will stat the target they point to
+  try {
+    const stats = await stat(path);
+
+    return {stat: stats, symbolicLink: lstats?.isSymbolicLink() ? {dangling: false} : undefined};
+  } catch (error) {
+    // If the link points to a non-existing file we still want
+    // to return it as result while setting dangling: true flag
+    if (error.code === 'ENOENT' && lstats) {
+      return {stat: lstats, symbolicLink: {dangling: true}};
+    }
+
+    throw error;
+  }
+}
+
+interface IStatAndLink {
+  // The stats of the file. If the file is a symbolic
+  // link, the stats will be of that target file and
+  // not the link itself.
+  // If the file is a symbolic link pointing to a non
+  // existing file, the stat will be of the link and
+  // the `dangling` flag will indicate this.
+  stat: fs.Stats;
+
+  // Will be provided if the resource is a symbolic link
+  // on disk. Use the `dangling` flag to find out if it
+  // points to a resource that does not exist on disk.
+  symbolicLink?: {dangling: boolean};
 }
